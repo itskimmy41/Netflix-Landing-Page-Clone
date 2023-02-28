cafd2ef30494884c3ba1c5b951543c13c83a9589
itskimmy41 <kimberlyfranczeska.iradiel@benilde.edu.ph>
2023-02-28 16:44:20 +0800

    add all files

---
+++ scripts.js
@@ -0,0 +1,40 @@
+let accordian = document.getElementsByClassName("FAQ-title");
+let dropdownBtn = document.getElementById("myDropdown");
+let dropdownBtn2 = document.getElementById("myDropdown2");
+
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+function myFunction() {
+  dropdownBtn.classList.toggle("show");
+  dropdownBtn2.classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
